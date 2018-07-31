41d120ad9a89051944f29f3d82582c9cebd20541
Aravinth <41901347+Aravinthr20@users.noreply.github.com>
2018-07-31 11:31:46 +0530

    Add files via upload

---
+++ charcheck.py
@@ -0,0 +1,7 @@
+y='a'
+if (y == 'a')or(y =='e')or(y =='i')or(y =='o')or(y=='u'):
+    print('VOWEL')
+elif (y.isalpha()):
+    print('Consonant')
+else:
+    print("Invalid")
---
+++ leapyear.py
@@ -0,0 +1,5 @@
+y=2016
+if((y%400==0) or (y%4==0) or (y%100 ==0)):
+    print('yes')
+else:
+    print('not')
